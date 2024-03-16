ec191a3446aabc8bde5c50fb86c903aa86da5e39
RavinduKavinda <dlravindukavinda@gmail.com>
2024-03-16 15:39:40 +0530

    nav bar setup

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -1,9 +1,19 @@
 import React from 'react'
 import './Navbar.css'
+import logo from './../../Assets/logo.png'
 
 const Navbar = () => {
   return (
-    <div>Navbar</div>
+    <nav className='container'>
+      <img src={logo} alt="" className='logo' />
+      <ul>
+        <li>Home</li>
+        <li>Services Us</li>
+        <li>Why Us</li>
+        <li>Our Goals</li>
+        <li><button className='btn'>Contact Us</button></li>
+      </ul>
+    </nav>
   )
 }
 
